8d1012d0205ab9219785fbf2da6b84c59657737c
Pho Hale <PhoHale@gmail.com>
2025-03-07 20:26:27 -0500

    more

--- neuropy/utils/mixins/dynamic_conformance_updating_mixin.py
+++ neuropy/utils/mixins/dynamic_conformance_updating_mixin.py
@@ -6,38 +6,60 @@ class BaseDynamicInstanceConformingMixin:
     
     from neuropy.utils.mixins.dynamic_conformance_updating_mixin import BaseDynamicInstanceConformingMixin
     
+    
+    History:
+	Generalized from:
+		[/c:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/General/Pipeline/Stages/Display.py:638](vscode://file/c:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/General/Pipeline/Stages/Display.py:638)
+		```python
+		# From `General.Pipeline.Stages.Display.PipelineWithDisplaySavingMixin`
+		PipelineWithDisplaySavingMixin
+		```
+
     """
     
     @classmethod
     def conform(cls, obj):
-        """ makes the object conform to this mixin by adding its methods and properties """
+        """ makes the object conform to this mixin by adding all its features """
         target_class = type(obj)
-        
-        # For methods
-        def conform_to_implementing_method(func):
-            setattr(target_class, func.__name__, func)
-        
-        # For properties
-        def conform_to_implementing_property(prop_name):
-            prop_obj = getattr(cls.__class__, prop_name)
-            setattr(target_class, prop_name, prop_obj)
-        
-        # Get all attributes from the mixin class
         base_attrs = dir(BaseDynamicInstanceConformingMixin)
         
-        # Handle methods
-        mixin_methods = [method for method in dir(cls) 
-                         if callable(getattr(cls, method)) 
-                         and method not in base_attrs 
-                         and not method.startswith('__')]
+        # Get mixin-specific attributes
+        mixin_attrs = [attr for attr in dir(cls) if attr not in base_attrs and not attr.startswith('__')]
         
-        for method_name in mixin_methods:
-            method = getattr(cls, method_name)
-            conform_to_implementing_method(method)
+        # Get class-level attributes (for class variables)
+        for attr_name in mixin_attrs:
+            # Handle different types of attributes
+            attr = getattr(cls, attr_name)
+            
+            # Method handling
+            if callable(attr):
+                if isinstance(attr, staticmethod):
+                    # Static methods
+                    setattr(target_class, attr_name, staticmethod(attr.__func__))
+                elif isinstance(attr, classmethod):
+                    # Class methods
+                    setattr(target_class, attr_name, classmethod(attr.__func__))
+                else:
+                    # Regular methods
+                    setattr(target_class, attr_name, attr)
+            
+            # Class variable handling (non-callable attributes)
+            else:
+                setattr(target_class, attr_name, attr)
         
-        # Handle properties
-        for attr_name in dir(cls.__class__):
-            if attr_name not in base_attrs and not attr_name.startswith('__'):
-                attr = getattr(cls.__class__, attr_name)
-                if isinstance(attr, property):
-                    conform_to_implementing_property(attr_name)
+        # Handle class-level special attributes (like properties)
+        cls_attrs = [attr for attr in dir(cls.__class__) 
+                    if attr not in dir(BaseDynamicInstanceConformingMixin.__class__) 
+                    and not attr.startswith('__')]
+        
+        for attr_name in cls_attrs:
+            attr = getattr(cls.__class__, attr_name)
+            
+            # Property handling
+            if isinstance(attr, property):
+                setattr(target_class, attr_name, attr)
+            
+            # Other descriptor handling
+            elif hasattr(attr, '__get__'):
+                setattr(target_class, attr_name, attr)
+                
