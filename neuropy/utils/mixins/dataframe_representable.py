60f5941f551fef2037508de53b9299bd65856faa
Pho Hale <PhoHale@gmail.com>
2025-09-11 15:45:44 -0400

    made `ensure_dataframe` a general function that works on any `DataFrameRepresentable`, instead of just Epoch objects

--- neuropy/utils/mixins/dataframe_representable.py
+++ neuropy/utils/mixins/dataframe_representable.py
@@ -1,6 +1,17 @@
 ## Implementors can be faithfully (although maybe not completely) represented by a Pandas DataFrame. 
 # dataframe_representable.py
 
+def ensure_dataframe(epochs: Union[DataFrameRepresentable, pd.DataFrame]) -> pd.DataFrame:
+    """ Ensures that the item are returned as an Pandas DataFrame, does nothing if they already are a DataFrame.
+    Based off of to `ensure_Epoch(...)`/`ensure_dataframe(...)`
+    Usage:
+        from neuropy.utils.mixins.dataframe_representable import ensure_dataframe
+    """
+    if isinstance(epochs, pd.DataFrame):
+        return epochs
+    else:
+        return epochs.to_dataframe()
+	
 
 class DataFrameInitializable:
 	""" Implementors can be initialized from a Pandas DataFrame. 
