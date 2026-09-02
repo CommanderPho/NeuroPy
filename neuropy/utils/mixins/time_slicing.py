fixed major bug involving `active_time_variable_name` for spikes in KDiba sessions that was breaking computation and yielding 0 spikes for all time bins

neuropy/utils/mixins/time_slicing.py
@@ -694,21 +694,23 @@ class TimePointEventAccessor(TimeColumnAliasesProtocol, TimeSlicableObjectProtoc
 
 
 
-
-
-
+# @function_attributes(short_name=None, tags=['UNUSED'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2026-09-02 14:47', related_items=[])
 def _compute_time_point_event_arbitrary_provided_epoch_ids(spk_df, provided_epochs_df, epoch_label_column_name=None, no_interval_fill_value=np.nan, override_time_variable_name=None, overlap_behavior=OverlappingIntervalsFallbackBehavior.ASSERT_FAIL, debug_print=False):
     """ Computes the appropriate IDs from provided_epochs_df for each spikes to be added as an identities column to spikes_df
     
     overlap_behavior: OverlappingIntervalsFallbackBehavior - If ASSERT_FAIL, an AssertionError will be thrown in the case that any of the intervals in provided_epochs_df overlap each other. Otherwise, if FALLBACK_TO_SLOW_SEARCH, a much slower search will be performed that will still work.
-    
+
+
+    #TODO 2026-09-02 14:48: - [ ] NOTE: commented out just because it was unused.
+
     Example:
         # np.shape(spk_times_arr): (16318817,), p.shape(pbe_start_stop_arr): (10960, 2), p.shape(pbe_identity_label): (10960,)
         spike_pbe_identity_arr # Elapsed Time (seconds) = 90.92654037475586, 93.46184754371643, 90.16610431671143 
     """
-    # spk_times_arr = spk_df.t_seconds.to_numpy()
-    # active_time_variable_name: str = (override_time_variable_name or spk_df.spikes.time_variable_name) # by default use spk_df.spikes.time_variable_name, but an optional override can be provided (to ensure compatibility with PBEs)
-    active_time_variable_name: str = (override_time_variable_name or spk_df.time_point_event.time_variable_name) # by default use spk_df.spikes.time_variable_name, but an optional override can be provided (to ensure compatibility with PBEs)
+    active_time_variable_name: str = (override_time_variable_name or spk_df.spikes.time_variable_name) # by default use spk_df.spikes.time_variable_name, but an optional override can be provided (to ensure compatibility with PBEs)
+    # active_time_variable_name: str = (override_time_variable_name or spk_df.time_point_event.time_variable_name) # #TODO 2026-09-02 14:41: - [ ] BUG - overriden for Bapun sessions I believe but broke KDiba sessions (each epoch had zero spikes)
+    if (active_time_variable_name not in spk_df.columns) and (spk_df.time_point_event.time_variable_name in spk_df.columns):
+        active_time_variable_name = spk_df.time_point_event.time_variable_name
 
     spk_times_arr = spk_df[active_time_variable_name].to_numpy()
     curr_epochs_start_stop_arr = provided_epochs_df[['start','stop']].to_numpy()
@@ -743,9 +745,12 @@ def _compute_spike_arbitrary_provided_epoch_ids(spk_df, provided_epochs_df, epoc
         return spike_epoch_identity_arr
     
     # spk_times_arr = spk_df.t_seconds.to_numpy()
-    # active_time_variable_name: str = (override_time_variable_name or spk_df.spikes.time_variable_name) # by default use spk_df.spikes.time_variable_name, but an optional override can be provided (to ensure compatibility with PBEs)
-    active_time_variable_name: str = (override_time_variable_name or spk_df.time_point_event.time_variable_name) # by default use spk_df.spikes.time_variable_name, but an optional override can be provided (to ensure compatibility with PBEs)
+    active_time_variable_name: str = (override_time_variable_name or spk_df.spikes.time_variable_name) # by default use spk_df.spikes.time_variable_name, but an optional override can be provided (to ensure compatibility with PBEs)
+    # active_time_variable_name: str = (override_time_variable_name or spk_df.time_point_event.time_variable_name) # #TODO 2026-09-02 14:41: - [ ] BUG - overriden for Bapun sessions I believe but broke KDiba sessions (each epoch had zero spikes)
+    if (active_time_variable_name not in spk_df.columns) and (spk_df.time_point_event.time_variable_name in spk_df.columns):
+        active_time_variable_name = spk_df.time_point_event.time_variable_name
 
+    # assert (active_time_variable_name in spk_df.columns), f"active_time_variable_name: '{active_time_variable_name}' not in spk_df.columns: {list(spk_df.columns)}"
     spk_times_arr = spk_df[active_time_variable_name].to_numpy()
     curr_epochs_start_stop_arr = provided_epochs_df[['start','stop']].to_numpy()
     if epoch_label_column_name is None: